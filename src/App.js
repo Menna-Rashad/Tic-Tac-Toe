ede7287d8bd453285ae159fcf85d35a6bae3cdea
Menna-Rashad <mennarashad301@gmail.com>
2026-02-16 20:23:08 +0200

    make the Square component and use it in the Board component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-export default function Square() {
-  return <> 
-  {/* why does the fragment opening tag needs to be in the same line as the return keyword for it to work?
-  we need the fragment opening next to the return because js engine considers the return statement already ended and it doesn't
-  read anything below it.
-  we also need the fragment element itself because in react, we retuen only one element
-  re can return in ( ) but we also need the fragment or any other element to swap the whole thing
-  just to make sure we're returning one single element*/}
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
+function Square (){
+  return <button className="square">1</button>;
+}
 
-  </>;
+
+export default function Board() {
+  return (
+    <> 
+      {/* why does the fragment opening tag needs to be in the same line as the return keyword for it to work?
+      we need the fragment opening next to the return because js engine considers the return statement already ended and it doesn't
+      read anything below it.
+      we also need the fragment element itself because in react, we retuen only one element
+      re can return in ( ) but we also need the fragment or any other element to swap the whole thing
+      just to make sure we're returning one single element*/}
+        <div className="board-row">
+          <Square/>
+          <Square/>
+          <Square/>
+        </div>
+        <div className="board-row">
+          <Square/>
+          <Square/>
+          <Square/>
+        </div>
+        <div className="board-row">
+          <Square/>
+          <Square/>
+          <Square/>
+        </div>
+    </>
+  )
+  ;
 }
 
 // why do i need the 'default' keyword here to tell other files using this file that it's the main function?
